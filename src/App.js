6a6965d8f08d39fd4cbd4187100fb8eb34b3603b
James Lo <jameslo599@gmail.com>
2023-12-28 00:20:21 -0600

    started front-end

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <MyHeader />
+    </div>
+  );
+}
+
+function MyHeader() {
+  return (
+    <div>
+      <header className="header">
+        <nav>
+          <div>
+            <a href="#">
+              <img className="logo" alt="capital one logo" src={logo}></img>
+            </a>
+          </div>
+        </nav>
       </header>
     </div>
   );
