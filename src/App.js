d73ffd334284ba648a6db193dc2d55b69d6f38ae
James Lo <jameslo599@gmail.com>
2023-12-30 00:57:57 -0600

    added banner and first tile

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="App">
       <MyHeader />
+      <MyBanner />
+      <section className="tile-layout">
+        <AccountTile />
+      </section>
     </div>
   );
 }
@@ -24,12 +28,57 @@ function MyHeader() {
           <div className="header-support">
             <button>
               <Comments />
-              <span>Need Help?</span>
+              <span className="supp-text">Need Help?</span>
             </button>
             <ISorting />
           </div>
         </nav>
       </header>
+    </div>
+  );
+}
+
+function MyBanner() {
+  return (
+    <div className="banner">
+      <p>
+        2023 tax forms will be available by January 31st.{" "}
+        <a href="#">Learn more.</a>
+      </p>
+    </div>
+  );
+}
+
+function AccountTile() {
+  return (
+    <div>
+      <div className="account-tile">
+        <div>
+          <div className="account-main">
+            <span>
+              360 Checking <span>...9473</span>
+            </span>
+            <div>
+              <span>$</span>0<span>00</span>
+            </div>
+            <span>AVAILABLE BALANCE</span>
+          </div>
+          <button className="account-button">View Account</button>
+        </div>
+        <div className="account-balance">
+          <div>
+            <span>$0.00</span>
+            <span>CURRENT BALANCE</span>
+          </div>
+        </div>
+      </div>
+      <div className="account-skip">
+        Skip the password -{" "}
+        <a href="#">
+          access your account info with face or fingerprint id when you use the
+          mobile app.
+        </a>
+      </div>
     </div>
   );
 }
