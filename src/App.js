7e5eda9df84accb6821532d267409113b37590d5
James Lo <jameslo599@gmail.com>
2024-01-03 00:59:48 -0600

    made footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from "./images/logo.svg";
 import credit from "./images/credit-score.png";
 import khan from "./images/money-swirl.avif";
 import macy from "./images/macys_legacy.webp";
-import map from "./images/location.png";
+import fdic from "./images/fdic.jpg";
+import house from "./images/house.jpg";
 import priceline from "./images/priceline.webp";
 import adidas from "./images/adidas.webp";
 import ISorting from "./images/icons/i-sorting";
 import Comments from "./images/icons/comments";
+import ShowToHide from "./images/icons/show-to-hide";
 
 function App() {
   return (
@@ -29,6 +31,7 @@ function App() {
           <div></div>
         </div>
       </section>
+      <MyFooter />
     </div>
   );
 }
@@ -224,6 +227,209 @@ function MapTile() {
           ></iframe>
         </div>
       </div>
+    </div>
+  );
+}
+
+function MyFooter() {
+  return (
+    <div>
+      <footer className="footer">
+        <div>
+          <ul className="footer-list-1">
+            <li>
+              <a
+                href="https://www.capitalone.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                PRODUCTS
+              </a>
+            </li>
+            <li className="sublist">
+              <ShowToHide text="ABOUT US" />
+              <ul>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/about/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    About Capital One
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://investor.capitalone.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Investors
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/about/newsroom/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Press
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/about/our-commitments/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Investing For Good
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>CAREERS</li>
+            <li className="sublist">
+              <ShowToHide text="LEGAL" />
+              <ul>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/military/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Servicemembers Civil Relief Act
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://ecm.capitalone.com/WCM/navigation/patriot-act-certification-cof-master_ada_2021.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Patriot Act Cert
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/digital/subpoena-policy/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Subpoena Policy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.capitalone.com/digital/disclosures/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Additional Disclosures
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="footer-list-2">
+            <li>
+              <a
+                href="https://www.capitalone.com/help-center/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                HELP
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/help-center/contact-us/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                CONTACT US
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/privacy/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                PRIVACY
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/digital/identity-protection/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                SECURITY
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/digital/terms-conditions/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                TERMS & CONDITIONS
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/accessibility/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                ACCESSIBILITY
+              </a>
+            </li>
+            <li>
+              <button>FEEDBACK</button>
+            </li>
+            <li>
+              <a
+                href="https://www.capitalone.com/digital/facts2019/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2019 CYBER INCIDENT
+              </a>
+            </li>
+          </ul>
+          <p>
+            Products and services are offered by Capital One, N.A., Member FDIC.
+            Deposit products are insured to{" "}
+            <a
+              href="https://www.capitalone.com/bank/money-management/banking-basics/fdic-insurance-limits/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              current FDIC limits.
+            </a>
+          </p>
+          <div className="gov">
+            <a href="https://www.fdic.gov/" target="_blank" rel="noreferrer">
+              <img src={fdic} alt="federal deposit insurance corporation"></img>
+            </a>
+            <a
+              href="https://www.occ.gov/publications-and-resources/publications/consumer-protection-pubs/files/equal-housing-lender.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={house} alt="equal housing lender"></img>
+            </a>
+            <a
+              href="https://www.jameshlo.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>© 2024 James H Lo</span>
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/images/icons/show-to-hide.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function ShowToHide(props) {
+  const text = props.text;
+  const fill = props.fill || "#011728";
+  const secondaryfill = props.secondaryfill || fill;
+  const strokewidth = props.strokewidth || 4;
+  const width = props.width || "100%";
+  const height = props.height || "100%";
+  const css = `.nc-int-show-to-hide{--transition-duration:0.4s}.nc-int-show-to-hide,.nc-int-show-to-hide *{transform-origin:50% 50%;transition:transform var(--transition-duration) cubic-bezier(.77,0,.18,1)}.nc-int-show-to-hide.nc-int-icon-state-b{transform:rotate(180deg)}.nc-int-show-to-hide.nc-int-icon-state-b :first-child{transform:rotate(90deg)}`;
+
+  function handleClick(e) {
+    let group = e.currentTarget.querySelector(".js-nc-int-icon");
+    if (!group) return;
+    group.classList.toggle("nc-int-icon-state-b");
+    e.currentTarget.dispatchEvent(new Event("ncstatechanged"));
+  }
+
+  return (
+    <button onClick={handleClick}>
+      <svg
+        className="expand"
+        height={height}
+        width={width}
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill={secondaryfill}
+          stroke={secondaryfill}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokewidth}
+        >
+          <g className="js-nc-int-icon nc-int-show-to-hide">
+            <line fill="none" x1="16" x2="16" y1="3" y2="29" />
+            <line fill="none" stroke={fill} x1="3" x2="29" y1="16" y2="16" />
+          </g>
+          <style>{css}</style>
+        </g>
+      </svg>
+      {text}
+    </button>
+  );
+}
+
+export default ShowToHide;
