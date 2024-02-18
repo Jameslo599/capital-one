55ec827f03bf8fbf04dfe804ab40618e82b10c57
James Lo <jameslo599@gmail.com>
2024-02-18 00:45:13 -0600

    added 404 page

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ learned git more effectively
 used MVC architecture with react
 implemented router abstraction in backend. got it to work client side through conditional rendering so the initial api call wasnt undefined
 It's to know that React runs the callback of an useEffect after all others normal JavaScript codes such as a console.log() and after the JSX is rendered. And even if that wasn't the case, a network request is asynchronous so you get the data after some delay.
+i love how in single page apps you can just import css to the root and it will apply to all pages
--- client/src/views/404.js
+++ client/src/views/404.js
@@ -1,11 +1,157 @@
-import "../styles/reset.scss";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+import cone from "../images/404.png";
+import fdic from "../images/fdic.jpg";
+import house from "../images/house.jpg";
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link to="/">OK</Link>
+    <div className="not-found">
+      <div>
+        <header className="header">
+          <nav>
+            <div>
+              <Link to="/">
+                <img className="logo" alt="capital one logo" src={logo}></img>
+              </Link>
+            </div>
+          </nav>
+        </header>
+      </div>
+      <section className="not-found">
+        <div className="message-box">
+          <div>
+            <img src={cone} alt="404 traffic cone"></img>
+          </div>
+          <h1>We can't find the page you're trying to reach.</h1>
+          <p>The link you are using may be outdated or misspelled.</p>
+          <button>
+            <Link to="/">OK</Link>
+          </button>
+        </div>
+      </section>
+      <div className="not-found footer-container">
+        <footer className="footer">
+          <div>
+            <ul className="footer-list-2">
+              <li>
+                <a
+                  href="https://www.capitalone.com/help-center/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  HELP
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/help-center/contact-us/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  CONTACT US
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/privacy/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  PRIVACY
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/digital/identity-protection/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SECURITY
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/digital/terms-conditions/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  TERMS & CONDITIONS
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/accessibility/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ACCESSIBILITY
+                </a>
+              </li>
+            </ul>
+            <ul className="footer-list-2 footer-list-3">
+              <li>
+                <a
+                  href="https://www.capitalone.com/military/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SERVICE MEMBERS CIVIL RELIEF ACT
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://ecm.capitalone.com/WCM/navigation/patriot-act-certification-cof-master_ada_2021.pdf"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  PATRIOT ACT CERT
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/digital/subpoena-policy/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SUBPOENA POLICY
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.capitalone.com/digital/disclosures/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ADDITIONAL DISCLOSURES
+                </a>
+              </li>
+            </ul>
+            <div className="gov">
+              <a href="https://www.fdic.gov/" target="_blank" rel="noreferrer">
+                <img
+                  src={fdic}
+                  alt="federal deposit insurance corporation"
+                ></img>
+              </a>
+              <a
+                href="https://www.occ.gov/publications-and-resources/publications/consumer-protection-pubs/files/equal-housing-lender.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={house} alt="equal housing lender"></img>
+              </a>
+              <a
+                href="https://www.jameshlo.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span>© 2024 James H Lo</span>
+              </a>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
